2acfaf2513afbc74089b24bed65cd7b4513d4b46
Ahmed Bouramdane <ahmedbourmdane77@gmail.com>
2025-10-31 12:48:50 +0100

    cpp programs

---
+++ basics/files/read_file.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    fstream file("file.txt");
+    string line;
+    while (getline(file, line))
+    {
+        cout << line << endl;
+    }
+    
+
+    file.close();
+    return 0;
+}
